9b39d2ba826a21926610e550fe3712095f51c3ae
mohamedbenaantar <mohamedbenantar@gmail.com>
2023-09-21 11:57:51 +0100

    Handle Routes

---
+++ airpodapp/src/routes/AllRoutes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Routes, Route} from "react-router-dom"
+import {Cart , Home} from "./../pages"
+export const AllRoutes = () => {
+  return (
+    <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="cart" element={<Cart/>}/>
+    </Routes>
+  )
+}
